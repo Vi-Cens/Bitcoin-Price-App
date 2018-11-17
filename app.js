2e35a4d76c0b3dca6c618b4e0481ad1be99998f0
unknown <pvicensleon@gmail.com>
2018-11-17 14:37:22 -0300

    add axios method, coindesk api

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 var url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
 
 //Axios GET method
+/*
 $("#axios").click(function(){
  axios.get(url)
 .then(function(res){
-    $("#quote").text(res.data[0]);
+    $("#price").text(res.data[0]);
   })
 .catch(function(err){
     console.log(err);
   })
+})
+*/
+
+//AXIOS get method
+axios.get(url)
+.then(function(res){
+	$("#price").text(res.data.bpi.USD.rate);  //data.bpi... from API
+})
+.catch(function(){
+	console.log("ERROR");
 })
